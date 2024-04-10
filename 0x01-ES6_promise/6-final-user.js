fd1d9fcda94e18d731a9937eb51e1616932ebf8e
MentalSep <chahidjava@hotmail.com>
2024-04-10 19:02:33 +0000

    add task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,10 +3,10 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => {
+    .then((values) => (
       values.map((element) => ({
         status: element.status,
         value: element.status === 'fulfilled' ? element.value : `${element.reason}`,
-      }));
-    });
+      }))
+    ));
 }
