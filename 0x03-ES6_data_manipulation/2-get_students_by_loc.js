c6f83a2430e6b8d425478bbad6537f403962e093
MentalSep <chahidjava@hotmail.com>
2024-04-15 19:17:00 +0100

    add task 2 and fix packages

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,6 @@
+export default function getStudentsByLocation(array, location) {
+  if (!Array.isArray(array)) {
+    return [];
+  }
+  return array.filter((student) => student.location === location);
+}
