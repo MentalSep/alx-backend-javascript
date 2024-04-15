95febd512a8f2c61ac4895b213d852372cd985c3
MentalSep <chahidjava@hotmail.com>
2024-04-15 20:36:33 +0100

    add task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,11 @@
 export default function cleanSet(set, startString) {
-  if (!set || !startString) return '';
+  if (!startString || startString.length === 0) {
+    return '';
+  }
 
-  const filteredValues = [...set].filter((value) => value.startsWith(startString));
-  const cleanValues = filteredValues.map((value) => value.slice(startString.length));
+  const cleanValues = [...set]
+    .filter((value) => (value ? value.startsWith(startString) : ''))
+    .map((value) => (value ? value.slice(startString.length) : ''));
 
   return cleanValues.join('-');
 }
