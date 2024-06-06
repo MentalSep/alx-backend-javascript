0ad17903d17029bb33c70e5234900d9aa8ccc9cd
MentalSep <chahidjava@hotmail.com>
2024-06-06 18:43:37 +0100

    add task 6

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,8 +1,8 @@
-const { expect } = require('chai');
+const expect = require('chai').expect;
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
-  it('should return a resolved promise with the correct data when success is true', () => new Promise((done) => {
+  it('should return a resolved promise with the correct data when success is true', (done) => {
     getPaymentTokenFromAPI(true)
       .then((data) => {
         expect(data).to.deep.equal({ data: 'Successful response from the API' });
@@ -11,5 +11,5 @@ describe('getPaymentTokenFromAPI', () => {
       .catch((error) => {
         done(error);
       });
-  }));
+  });
 });
