0a80de5c5711ba439a875156cf0260ef8d8248b4
MentalSep <chahidjava@hotmail.com>
2024-09-18 23:10:25 +0100

    fix task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,12 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
-process.stdin.once('data', (name) => {
+process.stdin.once("data", (name) => {
   process.stdout.write(`Your name is: ${name}`);
   process.stdin.destroy();
 });
 
-process.stdin.on('close', () => {
-  process.stdout.write('This important software is now closing\n');
+process.stdin.on("close", () => {
+  if (!process.stdin.isTTY) {
+    process.stdout.write("This important software is now closing\n");
+  }
 });
